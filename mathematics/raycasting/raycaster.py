754024dabf5bb3e4b742f4bb4f1c0951508ec5b1
PavelVavruska <vavruska.pavel@gmail.com>
2020-05-03 14:15:58 +0200

    *revert of math optimization

--- mathematics/raycasting/raycaster.py
+++ mathematics/raycasting/raycaster.py
@@ -1,17 +1,5 @@
 import math
 
-tan_from_degrees_10 = []
-for ray_angle in range(3600):
-    tan_from_degrees_10.append(math.tan(math.radians(ray_angle/10)))
-
-tan_from_degrees_10 = tuple(tan_from_degrees_10)
-
-cos_from_degrees = []
-for ray_angle in range(3600):
-    cos_from_degrees.append(math.cos(math.radians(ray_angle/10)))
-
-cos_from_degrees = tuple(cos_from_degrees)
-
 
 class Raycaster:
 
@@ -22,11 +10,11 @@ class Raycaster:
                                         config_is_perspective_correction_on,
                                         ):
         ray_position_for_map_collision_x = int(ray_position_x)
-        if 900 < ray_angle < 2700:
+        if 90 < ray_angle < 270:
             ray_position_for_map_collision_x = math.ceil(ray_position_x - 1)
         ray_position_for_map_collision_y = int(ray_position_y)
 
-        if 1800 < ray_angle < 3600:
+        if 180 < ray_angle < 360:
             ray_position_for_map_collision_y = math.ceil(ray_position_y - 1)
 
         if (0 <= ray_position_for_map_collision_x < game_map_size_x and
@@ -48,15 +36,15 @@ class Raycaster:
             if object_on_the_map_type_id < 0:  # floor/ceiling
                 object_type = 3
             elif 0 <= object_on_the_map_type_id < 10:
-                object_type = 1 # object
+                object_type = 1  # object
             else:
-                object_type = 2 # wall
+                object_type = 2  # wall
 
             if config_is_perspective_correction_on:
-                ray_perspective_correction_angle = ray_angle - player_angle*10
-                ray_distance_from_player_with_perspective_correction = cos_from_degrees[
-                    ray_perspective_correction_angle
-                ] * ray_distance_from_player
+                ray_perspective_correction_angle = ray_angle - player_angle
+                ray_distance_from_player_with_perspective_correction = math.cos(
+                    math.radians(ray_perspective_correction_angle)
+                ) * ray_distance_from_player
                 return (
                     object_type,
                     ray_distance_from_player_with_perspective_correction,
@@ -80,56 +68,55 @@ class Raycaster:
         # 180 - 270
         # 4
         # 270 - 360
-        ray_angle_div_10 = ray_angle/10
 
-        if 0 <= ray_angle <= 900:
+        if 0 <= ray_angle <= 90:
             ray_length_delta_x = 1 + int(ray_position_x) - ray_position_x
             ray_length_delta_y = 1 + int(ray_position_y) - ray_position_y
 
             ray_angle_to_tile_edge = math.degrees(math.atan(ray_length_delta_y / ray_length_delta_x))
 
-            if ray_angle_to_tile_edge >= ray_angle_div_10:
+            if ray_angle_to_tile_edge >= ray_angle:
                 ray_position_x = ray_position_x + ray_length_delta_x
-                ray_position_y += tan_from_degrees_10[ray_angle] * ray_length_delta_x
+                ray_position_y += math.tan(math.radians(ray_angle)) * ray_length_delta_x
             else:
-                ray_position_x += ray_length_delta_y / tan_from_degrees_10[ray_angle]
+                ray_position_x += ray_length_delta_y / math.tan(math.radians(ray_angle))
                 ray_position_y = ray_position_y + ray_length_delta_y
 
-        elif 900 < ray_angle < 1800:
+        elif 90 < ray_angle < 180:
             ray_length_delta_x = 1 - int(math.ceil(ray_position_x)) + ray_position_x
             ray_length_delta_y = 1 + int(ray_position_y) - ray_position_y
             ray_angle_to_tile_edge = 90 + math.degrees(math.atan(ray_length_delta_x / ray_length_delta_y))
 
-            if ray_angle_to_tile_edge <= ray_angle_div_10:
+            if ray_angle_to_tile_edge <= ray_angle:
                 ray_position_x = ray_position_x - ray_length_delta_x
-                ray_position_y += ray_length_delta_x / tan_from_degrees_10[ray_angle - 900]
+                ray_position_y += ray_length_delta_x / math.tan(math.radians(ray_angle - 90))
             else:
-                ray_position_x -= tan_from_degrees_10[ray_angle - 900] * ray_length_delta_y
+                ray_position_x -= math.tan(math.radians(ray_angle - 90)) * ray_length_delta_y
                 ray_position_y = ray_position_y + ray_length_delta_y
 
-        elif 1800 <= ray_angle < 2700:
+        elif 180 <= ray_angle < 270:
             ray_length_delta_x = 1 - int(math.ceil(ray_position_x)) + ray_position_x
             ray_length_delta_y = 1 - int(math.ceil(ray_position_y)) + ray_position_y
             ray_angle_to_tile_edge = 180 + math.degrees(math.atan(ray_length_delta_y / ray_length_delta_x))
 
-            if ray_angle_to_tile_edge > ray_angle_div_10:
+            if ray_angle_to_tile_edge > ray_angle:
                 ray_position_x = ray_position_x - ray_length_delta_x
-                ray_position_y -= tan_from_degrees_10[ray_angle - 1800] * ray_length_delta_x
+                ray_position_y -= math.tan(math.radians(ray_angle - 180)) * ray_length_delta_x
             else:
-                ray_position_x -= ray_length_delta_y / tan_from_degrees_10[ray_angle - 1800]
+                ray_position_x -= ray_length_delta_y / math.tan(math.radians(ray_angle - 180))
                 ray_position_y = ray_position_y - ray_length_delta_y
 
-        elif 2700 <= ray_angle < 3600:
+        elif 270 <= ray_angle < 360:
             ray_length_delta_x = 1 + int(ray_position_x) - ray_position_x
             ray_length_delta_y = 1 - int(math.ceil(ray_position_y)) + ray_position_y
             ray_angle_to_tile_edge = 270 + math.degrees(math.atan(ray_length_delta_x / ray_length_delta_y))
 
-            if ray_angle_to_tile_edge > ray_angle_div_10:
-                ray_position_x += tan_from_degrees_10[ray_angle - 2700] * ray_length_delta_y
+            if ray_angle_to_tile_edge > ray_angle:
+                ray_position_x += math.tan(math.radians(ray_angle - 270)) * ray_length_delta_y
                 ray_position_y = ray_position_y - ray_length_delta_y
             else:
                 ray_position_x = ray_position_x + ray_length_delta_x
-                ray_position_y -= ray_length_delta_x / tan_from_degrees_10[ray_angle - 2700]
+                ray_position_y -= ray_length_delta_x / math.tan(math.radians(ray_angle - 270))
         return ray_position_x, ray_position_y
 
     @classmethod
@@ -141,10 +128,10 @@ class Raycaster:
         player_angle_start = player_angle - config_fov / 2
         x_cor_z_buffer_objects = []
         for screen_x in range(0, int(mini_map_offset_x), config_pixel_size):
-            ray_angle = int((player_angle_start + config_fov / mini_map_offset_x * screen_x)*10)
+            ray_angle = player_angle_start + config_fov / mini_map_offset_x * screen_x
 
             # degrees fixed to range 0 - 359
-            ray_angle %= 3600
+            ray_angle %= 360
 
             ray_position_x = player_pos_x  # start position of ray on X axis
             ray_position_y = player_pos_y  # start position of ray on Y axis
@@ -163,7 +150,7 @@ class Raycaster:
                 )
                 if detected:
                     object_type, ray_distance_from_player, object_on_the_map_type_id_with_offset = detected
-                    z_buffer_objects.append((ray_distance_from_player, object_on_the_map_type_id_with_offset, object_type, ray_angle/10))
+                    z_buffer_objects.append((ray_distance_from_player, object_on_the_map_type_id_with_offset, object_type, ray_angle))
                     if object_type == 2:  # is wall
                         break  # cannot see behind the first wall
 
